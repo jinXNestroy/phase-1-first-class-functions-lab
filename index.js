4d7a6708a360819f90672c28d799f2610596e07c
DanielJoel <danpersie53@gmail.com>
2024-03-30 00:13:12 +0300

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function (scuberDrivers) {
+    let firstTwo = scuberDrivers.slice(0, 2)
+    return firstTwo
+
+}
+
+const returnLastTwoDrivers = function (scuberDrivers) {
+    let lastTwo = scuberDrivers.slice(2)
+    return lastTwo
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num) {
+    let fareMultiplier = function (fare) {
+        let fareM = fare * num
+        return fareM
+    }
+    return fareMultiplier
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(scuberDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(scuberDrivers)
+
+}
